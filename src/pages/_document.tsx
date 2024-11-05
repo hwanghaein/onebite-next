3bfd9b824b85a7a33232637bfbdf55cf08332edb
황혜인 <hhi9037@naver.com>
2024-11-05 22:30:37 +0900

    Page Router를 위한 프로젝트 설치 및 구성

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// Next App에 모든 페이지에 공통적으로 적용될 로직, 레이아웃, 데이터를 다루는 파일
+
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="kr">
       <Head />
       <body>
         <Main />
@@ -11,3 +13,5 @@ export default function Document() {
     </Html>
   );
 }
+
+
