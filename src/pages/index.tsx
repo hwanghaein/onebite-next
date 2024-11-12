c10c371fb25bf6578276d194387a1d6e13febfd8
황혜인 <hhi9037@naver.com>
2024-11-12 22:26:55 +0900

    서버사이드렌더링 실습 - '등록된 모든 도서' 데이터 가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,36 @@
 import SearchableLayout from '@/components/searchable-layout';
 import style from "./index.module.css";
 import { ReactNode } from 'react';
-import books from '@/mock/books.json'
+// import books from '@/mock/books.json'
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
 
-export default function Home() {
+export const getServerSideProps = async() => {
+  // 컴포넌트보다 먼저 실행돼서, 컴포넌트에 필요한 데이터를 불러오는 함수
+  const allBooks = await fetchBooks()
+  return {
+    props: {
+      allBooks
+    },
+  };
+};
+
+export default function Home({ allBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+
   return (
     <div className={style.container}>
       <section >
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} data={book} />
         ))}
 
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} data={book} />
         ))}
 
