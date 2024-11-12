d2b155e9d251fca1b4b801eb61e1f805f98f60a8
황혜인 <hhi9037@naver.com>
2024-11-12 23:02:21 +0900

    서버사이드렌더링 실습 - 검색창 데이터

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,15 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from 'react';
-import books from '@/mock/books.json'
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
 
-export default function Page() {
-return<div>
-          {books.map((book) => (
-          <BookItem key={book.id} data={book} />
-        ))}
-</div>
+  return {
+    props: { books, },
+  };
+};
+
+export default function Page({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <div>
+    {books.map((book) => (
+      <BookItem key={book.id} data={book} />
+    ))}
+  </div>
 }
 
 Page.getLayout = (page: ReactNode) => {
