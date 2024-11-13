7f78cbdbe5325b26affbe3fb1300cdaec7a91a5e
황혜인 <hhi9037@naver.com>
2024-11-14 00:15:16 +0900

    fallback 옵션: true

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,14 +1,15 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from 'next/router';
 
 
 export const getStaticPaths = () => {
   return {
     // 어떤 경로들(URL 파라미터들)이 존재할 수 있는지 배열로 반환
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }, { params: { id: "3" } }], 
-    fallback: 
-    'blocking', // 보험, 대비책 -> SSR처럼 즉각적 페이지 생성해서 리턴해줌, 새로운 데이터(도서)가 계속 추가되어야하는 상황에도 사용이 가능하다.
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }, { params: { id: "3" } }],
+    fallback:
+      true, // SSR 방식 + 데이터가 없는 fallback 상태의 페이지부터 보내주고, 데이터는 후속으로 보내줌
   }
 }
 
@@ -16,12 +17,22 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; // 타입 단언을 써도 안전한 이유는 [id].tsx 페이지는 무조건 URL 파라미터가 하나 있어야만 접근할 수 있는 페이지이기 때문임
   const book = await fetchOneBook(Number(id))
 
+  if (!book) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
-    props: { book, }, 
+    props: { book, },
   };
 }
 
 export default function Page({ book, }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중입니다"
+
   if (!book) return "문제가 발생했습니다. 다시 시도하세요."
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
