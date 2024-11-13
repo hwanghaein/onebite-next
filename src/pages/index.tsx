9ae3eda291faef9c50a5a6fa1f749c48bc99d4cc
황혜인 <hhi9037@naver.com>
2024-11-14 02:22:11 +0900

    SEO 설정하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ import BookItem from '@/components/book-item';
 import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
+import Head from 'next/head';
 
 export const getStaticProps = async () => {
-  console.log("인덱스페이지");
-  
+
   // 컴포넌트보다 먼저 실행돼서, 컴포넌트에 필요한 데이터를 불러오는 함수
   const [allBooks, recoBooks] = await Promise.all([
     // 인수로 전달한 배열안에 들어있는 모든 비동기 함수들을 동시에 실행시켜주는 메서드
@@ -21,7 +21,6 @@ export const getStaticProps = async () => {
       allBooks,
       recoBooks
     },
-    revalidate : 3, // (3초 주기로) 재검증하다, 재생성하다. 
   };
 };
 
@@ -29,23 +28,31 @@ export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<ty
 
 
   return (
-    <div className={style.container}>
-      <section >
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} data={book} />
-        ))}
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property='og:image' content='/thumbnail.png' />
+        <meta property='og:title' content='한입북스' />
+        <meta property='og:description' content="한입 북스에 등록된 도서들을 만나보세요" />
+      </Head>
+      <div className={style.container}>
+        <section >
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} data={book} />
+          ))}
 
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} data={book} />
-        ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} data={book} />
+          ))}
 
 
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
 
